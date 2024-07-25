dddff123aee7b66dba4632a14c507e255f266d7f
songawon <ok466sgw@naver.com>
2024-07-25 21:21:24 +0900

    1

--- src/main/java/com/example/finalproject/FinalprojectApplication.java
+++ src/main/java/com/example/finalproject/FinalprojectApplication.java
@@ -2,8 +2,11 @@ package com.example.finalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+
+@EnableMongoRepositories(basePackages = "vacation.repository")
+@SpringBootApplication(scanBasePackages = {"com.example.finalproject", "vacation"})
 public class FinalprojectApplication {
 
     public static void main(String[] args) {
